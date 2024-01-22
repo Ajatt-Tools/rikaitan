837b6c45f63601e33bdef666cdf7f717e28b16ae
Ren Tatsumoto <tatsu@autistici.org>
2024-01-22 10:29:54 +0300

    rename file after build

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 rm -v -rf -- ./builds/*
 mkdir -p -- ./builds
 npm run-script build -- --all --version "$(./bump_ver)" "$@"
+mv -- ./builds/rikaitan-firefox-dev.zip ./builds/rikaitan-firefox-dev.xpi
