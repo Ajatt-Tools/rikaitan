8af46a92f27de0d1090de7815aae24daf72af5ee
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2024-02-27 07:23:42 -0500

    Object utilities
    * Create utilities
    
    * Rename old isObject
    
    * Use new isObject
    
    * Remove old function
    
    * Add additional function
    
    * Simplify for now
    
    * Rename function for clarity
    
    * Rename function
    
    * Expand type
    
    * Update eslint
    
    <rikaitan.link>H4sIAAAAAAAAAwXBQQEAIAgDwL9pUBAxDuDWP4J3sJNCi6C+W9V1HNU07iSWaE9AvGV8rHloACkAAAA=</rikaitan.link>

--- ext/js/data/options-util.js
+++ ext/js/data/options-util.js
@@ -18,7 +18,8 @@
 
 import {fetchJson, fetchText} from '../core/fetch-utilities.js';
 import {parseJson} from '../core/json.js';
-import {escapeRegExp, isObject} from '../core/utilities.js';
+import {isObjectNotArray} from '../core/object-utilities.js';
+import {escapeRegExp} from '../core/utilities.js';
 import {TemplatePatcher} from '../templates/template-patcher.js';
 import {JsonSchema} from './json-schema.js';
 
@@ -70,7 +71,7 @@ export class OptionsUtil {
         // Remove invalid profiles
         const profiles = /** @type {unknown[]} */ (options.profiles);
         for (let i = profiles.length - 1; i >= 0; --i) {
-            if (!isObject(profiles[i])) {
+            if (!isObjectNotArray(profiles[i])) {
                 profiles.splice(i, 1);
             }
         }
