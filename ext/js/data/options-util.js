e0ceff94162e1bf733910a14262ae0bcf311bd91
Jason Ojisan <jason@fedora.email>
2025-09-11 10:58:57 -0500

    Add custom target tags functionality
    * Add custom target tags functionality
    
    * fix blank line for linter
    
    ---------
    
    Signed-off-by: Ryan Gisleson <rgisleson@gmail.com>
    
    <rikaitan.link>NTZlNzM1MGMzMjhiZWUxMWE0YTk1OTc2Y2FlMmNkYmQ4ZTZmYTlkZgo=</rikaitan.link>

--- ext/js/data/options-util.js
+++ ext/js/data/options-util.js
@@ -584,6 +584,7 @@ export class OptionsUtil {
             this._updateVersion70,
             this._updateVersion71,
             this._updateVersion72,
+            this._updateVersion73,
         ];
         /* eslint-enable @typescript-eslint/unbound-method */
         if (typeof targetVersion === 'number' && targetVersion < result.length) {
@@ -1795,6 +1796,16 @@ export class OptionsUtil {
      */
     async _updateVersion72(options) {
         await this._applyAnkiFieldTemplatesPatch(options, '/data/templates/anki-field-templates-upgrade-v71.handlebars');
+    }
+
+    /**
+     *  - Added anki.targetTags
+     *  @type {import('options-util').UpdateFunction}
+     */
+    async _updateVersion73(options) {
+        for (const profile of options.profiles) {
+            profile.options.anki.targetTags = [];
+        }
     }
 
     /**
